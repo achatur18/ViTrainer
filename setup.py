061a2ab9534e5fac14ccdb8f02e9dddd74886493
achatur18 <abhay.chaturvedi@awone.ai>
2023-01-12 14:37:15 +0530

    setup file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ViTrainer',
+    version='0.1',
+    packages=find_packages(),
+    url='https://github.com/achatur18/ViTrainer',
+    license='MIT',
+    author='achatur18',
+    author_email='abhay.chaturvedi@awone.ai',
+    description='A single library for training your computer vision models.',
+    install_requires=[
+        'numpy',
+        "torch"
+    ],
+)
