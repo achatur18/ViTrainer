f7973ed1a0367aae379061671294de0a5a74ac16
achatur18 <abhay.chaturvedi@awone.ai>
2023-01-12 13:37:19 +0530

    modified:   src/train_classification.py

--- src/train_classification.py
+++ src/train_classification.py
@@ -1,26 +1,17 @@
-from torch.utils.data import DataLoader
-from torchvision.transforms import (
-    Compose,
-    RandomResizedCrop,
-    RandomHorizontalFlip,
-    ToTensor,
-    Resize,
-    CenterCrop,
-)
-
-
-from torchvision.datasets import ImageFolder
-import yaml
 import os
 
-import torch
-import torch.nn as nn
-
-import torchvision
-
-import numpy as np
+import colorama
 import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision
+import yaml
+from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
+from torchvision.transforms import (CenterCrop, Compose, RandomHorizontalFlip,
+                                    RandomResizedCrop, Resize, ToTensor)
 
 matplotlib.rc("font", size=16)
 
@@ -33,9 +24,28 @@ def get_config(config_path):
 
 def train(config_path):
     config = get_config(config_path)
+
+    # Create datasets
+    train_data = ImageFolder(
+        model["dataset"]["train_path"],
+        transform=Compose(
+            [RandomResizedCrop(224), RandomHorizontalFlip(),
+             ToTensor()]  # data augmentation
+        ),
+    )
+
+    test_data = ImageFolder(
+        model["dataset"]["test_path"],
+        # give images the same size as the train images
+        transform=Compose([Resize(256), CenterCrop(224), ToTensor()]),
+    )
+
+    # Our datasets have two classes:
+    class_names = train_data.classes
+
     model = torchvision.models.resnet18(pretrained=True)
     model.fc = nn.Linear(
-        in_features=512, out_features=config["model"]["output_neurons"]
+        in_features=512, out_features=len(class_names)
     )
 
 
